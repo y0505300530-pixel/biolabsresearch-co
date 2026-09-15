3c6d417b7c414348650c4b5caf23eb69706615a8
Meir Kaufman <admin@biolabsresearch.co>
2026-09-15 23:27:23 +0000

    v2.81: GLOW 70 black-name + gold 70 MG bar; media + ?v=173 bust

--- html/mg-picker.js
+++ html/mg-picker.js
@@ -37,7 +37,7 @@
     return n.replace("mg", " mg");
   }
   function fileFor(slug) {
-    return "/media/vial-" + slug + ".png?v=172";
+    return "/media/vial-" + slug + ".png?v=173";
   }
   function baseName(name) {
     return String(name || "").replace(/\s*\([^)]*mg[^)]*\)\s*$/i, "").trim();
